2901dd8c42f2483cd35656fd19c328c9a4158fb9
Irina Plaksina <irene.plaksina@gmail.com>
2020-04-14 21:18:07 -0400

    user's inpur saved to local storage

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ $("#add-task").on("click", function () {
 //   display the new task block
     $("#new-task").removeClass("hide");
 });
+
+//set local storage based on user's input
+$("#save").on("click", function () {
+    let newTime = $("option").val();
+    let newTask = $("#task").val();
+    console.log("time ", newTime, "task: ", newTask);
+localStorage.setItem(newTime, newTask);
+});
